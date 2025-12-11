d929e483acc2a8106d2648ae7d20561088d22040
shraddhagurav2002-hub <shraddhagurav2002@gmail.com>
2025-12-11 16:21:34 +0530

    Update package installation options and settings
    Set CRAN repository and prevent installation of raster/terra on Render.

--- install_packages.R
+++ install_packages.R
@@ -1,5 +1,22 @@
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+# Prevent installing raster/terra on Render
+Sys.setenv("_R_CHECK_FORCE_SUGGESTS_" = "false")
+Sys.setenv("PKG_CONFIG_PATH" = "")
+
 install.packages(c(
   "shiny","RPostgres","DBI","dplyr","ggplot2","plotly",
   "leaflet","DT","bslib","shinyWidgets","thematic",
   "shinyjs","lubridate","shinycssloaders","tools"
-), repos = "https://cloud.r-project.org")
+))
+
+
+
+
+
+
+
+
+
+
+
